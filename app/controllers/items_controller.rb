7d8cfc1decf0bfd2606427894d6d6cae5fe3e143
tame3239 <102791146+tame3239@users.noreply.github.com>
2022-05-07 14:35:07 +0900

    ActiveStorageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+ 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :item_condition_id, :payer_id, :prefectures_id, :waiting_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
