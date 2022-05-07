7229640a57b7ec5ef00ac32d1188dac0e92d3d49
tame3239 <102791146+tame3239@users.noreply.github.com>
2022-05-07 17:38:54 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :payer
+  belongs_to :prefectures
+  belongs_to :waiting_days
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, :item_condition_id,:payer_id, :prefectures_id, :waiting_days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
