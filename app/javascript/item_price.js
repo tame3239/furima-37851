04c6e14c9ebb87d887a1e0eded1c994a41dc5d49
tame3239 <102791146+tame3239@users.noreply.github.com>
2022-05-09 01:03:44 +0900

    テスト追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,15 +3,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
      const inputValue = priceInput.value;
-     //console.log(inputValue);
 
      const addTaxDom = document.getElementById("add-tax-price");
      const addTaxValue=(Math.floor(inputValue * 0.1));
      addTaxDom.innerHTML = addTaxValue;
-     //console.log(addTaxDom);
 
      const profitNumber = document.getElementById("profit")
      profitNumber.innerHTML = (Math.floor(inputValue - addTaxValue));
-       //console.log(profitNumber);
   })
 });
