18be507436fb34e752d33c9df8a0f4daee4f209b
tame3239 <102791146+tame3239@users.noreply.github.com>
2022-05-10 18:41:35 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :damedayo_edit, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :item_condition_id, :payer_id, :prefectures_id,
                                  :waiting_days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def damedayo_edit
+    @item = Item.find(params[:id])
+    @user = @item.user
+    redirect_to(root_path) unless @user == current_user
   end
 end
