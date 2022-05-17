29e308e5f6af08246bb13dfaa7c779121b108316
tame3239 <102791146+tame3239@users.noreply.github.com>
2022-05-17 10:43:23 +0900

    コメントモデル、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create]
   end
+  resources :items do
+    resources :comments, only: :create
+  end
+  
 end
